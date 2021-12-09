c53bff52fd51ec963597dfaaefbe8588894c2dc5
JRajis <94915526+JRajis@users.noreply.github.com>
2021-12-09 15:16:18 +0200

    Update models.py

--- board_game_groups/models.py
+++ board_game_groups/models.py
@@ -3,14 +3,19 @@ from django.db import models
 # Create your models here.
 class Game(models.Model):
   Game_name = models.CharField(max_length=300)
+  Owner = models.ForeignKey(Gamer, on_delete=models.CASCADE,)
+  
+  return self.Borrow_Date
   
 class Gamer(models.Model):
   Name = CharField(max_length=100)
   
+  
 class Loans(models.Model):
+  Borrow_Date = models.DateTimeField(auto_now_add=True)
   Borrower = models.ForeignKey(Gamer, on_delete=models.CASCADE,)
   Borrowed_Game = models.ForeignKey(Game, on_delete=models.CASCADE,)
   
   def __str__(self):
-    return self.Borrower
+    return self.Borrow_Date
   
