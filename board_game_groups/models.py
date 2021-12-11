81b8d6d5f324e5ae3248e330e848d130a2b677a2
JRajis <94915526+JRajis@users.noreply.github.com>
2021-12-11 15:55:40 +0200

    Update models.py

--- board_game_groups/models.py
+++ board_game_groups/models.py
@@ -2,21 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-# Class for the players.
-class Gamer(models.Model):
-  Name = models.CharField(max_length=100)
-  Owned_games = models.JSONField(default=list)
-  
-  def __str__(self):
-    return self.Name
-  
 # Class for the boardgames.
 class Game(models.Model):
   Game_name = models.CharField(max_length=300)
   Owner = models.CharField(max_length=30)
   Game_description = models.TextField()
-  Latest_borrower =  models.ForeignKey(Gamer, on_delete=models.CASCADE,)
   is_borrowed = models.BooleanField()
+  date_added = models.DateTimeField(auto_now_add=True)
   date_modified = models.DateTimeField(auto_now_add=True)
   
   
@@ -25,18 +17,23 @@ class Game(models.Model):
   
  
   
- 
-  
-
-  
-  """
-  
-class Loans(models.Model):
-  Borrow_Date = models.DateTimeField(auto_now_add=True)
-  Borrower = models.ForeignKey(Gamer, on_delete=models.CASCADE,)
+ class Borrow(models.Model):
   Borrowed_Game = models.ForeignKey(Game, on_delete=models.CASCADE,)
+  Borrow_date = models.DateTimeField(auto_now_add_True)
+  Planned_return_date = CharField(max_length=20)
   
   def __str__(self):
-    return self.Borrow_Date
+    return self.Borrowed_Game
   
 """
+# Class for the players. (currently unused)
+class Gamer(models.Model):
+  Name = models.CharField(max_length=100)
+  Owned_games = models.JSONField(default=list)
+  Contacts = models.TextField()
+  
+  def __str__(self):
+    return self.Name
+  """
+  
+  
